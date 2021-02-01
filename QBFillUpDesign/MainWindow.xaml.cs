1f9bca9b3c4e8afd18cb61985d7ad7a66ce41a73
Examsign.keerthana <Examsign.keerthana@outlook.com>
2021-02-01 16:49:53 +0530

    Add project files.

---
+++ QBFillUpDesign/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QBFillUpDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBlank_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPreview_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddImage_Click(object sender, RoutedEventArgs e)
+        {
+           // lstboxImgPath
+        }
+
+        private string BrowseImagePath()
+        {
+            string fileName = "";
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                fileName = openFileDialog.FileName;
+            }
+
+            return fileName;
+        }
+
+        private string ConnectionString()
+        {
+            
+        }
+    }
+}
